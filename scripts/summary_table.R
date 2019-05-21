e8154a8dd7d6debd1bd4519440244f7501fd510d
Cameron Sims <simsc3@uw.edu>
2019-05-21 16:32:26 -0700

    Update column names in summary table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -18,6 +18,9 @@ summary_table <- function(df) {
     filter(appearances == max(appearances, na.rm = T)) %>% 
     distinct(decade, total_albums, Artist, average_song_rank, appearances) %>% 
     mutate(most_popular = toString(Artist)) %>% 
-    distinct(decade, total_albums, average_song_rank, most_popular)
+    distinct(decade, total_albums, average_song_rank, most_popular) %>% 
+    rename("Decade" = decade, "Total Albums" = total_albums,
+           "Average Song Rank" = average_song_rank,
+           "Most Popular Artist(s)" = most_popular)
   return(table)
 }
