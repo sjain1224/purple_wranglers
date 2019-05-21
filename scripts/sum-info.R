7eac6512ea4276ec1527425fac07b3db20c7c522
Sophie W <sfw6@uw.edu>
2019-05-21 14:55:53 -0700

    finish summary info (coming back to lintr later)

--- scripts/sum-info.R
+++ scripts/sum-info.R
@@ -1,7 +1,6 @@
 # Summary Info
 library(dplyr)
 
-test_df <- read.csv("data/albumlist.csv")
 get_summary_info <- function(data_set) {
   artist_1 <- data_set %>%
     filter(Number == 1) %>%
@@ -10,18 +9,19 @@ get_summary_info <- function(data_set) {
     filter(Number == 1) %>%
     pull(Album)
   artist_most_num <- data_set %>%
-    group_by(artist) %>%
-    mutate(num_albums = n()) %>%
+    group_by(Artist) %>%
+    summarize(num_albums = n()) %>%
     filter(num_albums == max(num_albums)) %>%
-    pull(artist)
+    pull(Artist)
   most_recent_album <- data_set %>%
     filter(Year == max(Year)) %>%
     pull(Album)
   year_with_most <- data_set %>%
     group_by(Year) %>%
-    mutate(num_albums = n()) %>%
+    summarize(num_albums = n()) %>%
     filter(num_albums == max(num_albums)) %>%
     pull(Year)
-  
+ return(list(art_1 = artist_1, alb_1 = album_1,
+             art_most_num = artist_most_num, most_recent = most_recent_album,
+             most_year = year_with_most)) 
 }
-
