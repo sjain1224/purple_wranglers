85bd07631d61013f6f9826b3617020e7d547b5de
Sophie W <sfw6@uw.edu>
2019-05-22 12:02:51 -0700

    sum-info add more variables

--- scripts/sum-info.R
+++ scripts/sum-info.R
@@ -1,6 +1,7 @@
 # Summary Info
 library(dplyr)
 
+test_df <- read.csv("../data/albumlist.csv")
 get_summary_info <- function(data_set) {
   artist_1 <- data_set %>%
     filter(Number == 1) %>%
@@ -8,6 +9,9 @@ get_summary_info <- function(data_set) {
   album_1 <- data_set %>%
     filter(Number == 1) %>%
     pull(Album)
+  year_1 <- data_set %>%
+    filter(Number == 1) %>%
+    pull(Year)
   artist_most_num <- data_set %>%
     group_by(Artist) %>%
     summarize(num_albums = n()) %>%
@@ -16,12 +20,19 @@ get_summary_info <- function(data_set) {
   most_recent_album <- data_set %>%
     filter(Year == max(Year)) %>%
     pull(Album)
+  most_recent_artist <- data_set %>%
+    filter(Year == max(Year)) %>%
+    pull(Artist)
+  most_recent_year <- data_set %>%
+    filter(Year == max(Year)) %>%
+    pull(Year)
   year_with_most <- data_set %>%
     group_by(Year) %>%
     summarize(num_albums = n()) %>%
     filter(num_albums == max(num_albums)) %>%
     pull(Year)
- return(list(art_1 = artist_1, alb_1 = album_1,
+ return(list(art_1 = artist_1, alb_1 = album_1, year_1 = year_1,
              art_most_num = artist_most_num, most_recent = most_recent_album,
-             most_year = year_with_most)) 
+             most_year = year_with_most, recent_year = most_recent_year,
+             recent_art = most_recent_artist))
 }
