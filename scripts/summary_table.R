13ebbd214ce855f379b3ec7871f7636bfd60a158
Cameron Sims <simsc3@uw.edu>
2019-05-22 08:55:14 -0700

    Update summary_table to follow lintr styling

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -8,18 +8,18 @@ library("dplyr")
 # its most popular artist(s).
 summary_table <- function(df) {
   table <- df %>%
-    mutate("decade" = floor(Year / 10)*10) %>% 
-    group_by(decade) %>% 
-    add_count(decade, name = "total_albums") %>% 
-    mutate("average_song_rank" = round(mean(Number, na.rm = T),2)) %>% 
-    group_by(decade, Artist) %>% 
+    mutate("decade" = floor(Year / 10) * 10) %>%
+    group_by(decade) %>%
+    add_count(decade, name = "total_albums") %>%
+    mutate("average_song_rank" = round(mean(Number, na.rm = T), 2)) %>%
+    group_by(decade, Artist) %>%
     add_count(Artist, name = "appearances") %>%
-    group_by(decade) %>% 
-    filter(appearances == max(appearances, na.rm = T)) %>% 
-    distinct(decade, total_albums, Artist, average_song_rank, appearances) %>% 
-    mutate(most_popular = toString(Artist)) %>% 
-    arrange(decade) %>% 
-    distinct(decade, total_albums, average_song_rank, most_popular) %>% 
+    group_by(decade) %>%
+    filter(appearances == max(appearances, na.rm = T)) %>%
+    distinct(decade, total_albums, Artist, average_song_rank, appearances) %>%
+    mutate(most_popular = toString(Artist)) %>%
+    arrange(decade) %>%
+    distinct(decade, total_albums, average_song_rank, most_popular) %>%
     rename("Decade" = decade, "Total Albums" = total_albums,
            "Average Song Rank" = average_song_rank,
            "Most Popular Artist(s)" = most_popular)
