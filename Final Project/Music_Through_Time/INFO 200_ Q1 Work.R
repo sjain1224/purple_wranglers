a04e54a1923de95744e9ae4ee9f7f33fa96ad5ab
Sophie W <sfw6@uw.edu>
2019-06-04 15:07:26 -0700

    trying new graph

---
+++ Final Project/Music_Through_Time/INFO 200_ Q1 Work.R
@@ -0,0 +1,134 @@
+# UI STUFF
+
+
+library(ggvis)
+
+axis_vars <- list("Year" = "year", "Rank" = "rank", "Album Length (minutes)" = "track_min")
+
+
+fluidPage(
+  titlePanel("Do Track Number or Length Matter?"),
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Filter Fields"),
+             sliderInput("one_min_length", "Minimum length of album (in minutes)",
+                         3, 800, 100, step = 5),
+             sliderInput("one_track_num", "Minimum number of tracks on album",
+                         0, 700, 18, step = 1),
+             sliderInput("one_year", "Year on Billboard Top 200", 1963, 2019, value = c(1963, 2019),
+                         sep = ""),
+             sliderInput("one_time_on", "Number of weeks on Billboard Top 200",
+                         0, 52, c(0, 52), step = 1)
+           ),
+           wellPanel(
+             selectInput("xvar", "X-axis variable", axis_vars, selected = "year"),
+             selectInput("yvar", "Y-axis variable", axis_vars, selected = "track_min"),
+             tags$small(paste0(
+               "Note: Some help found through movie explorer",
+               "shiny from R studio help graph by Winston Chang"
+             ))
+           )
+    ),
+    column(9,
+           ggvisOutput("plot1")
+    )
+  )
+)
+
+
+
+# SERVER STUFF
+
+library(ggvis)
+library(dplyr)
+
+# Set up handles to database tables on app start
+one_data <- read.csv("../../data/billboardalbum200.csv", stringsAsFactors = F)
+one_data <- one_data %>%
+  mutate(track_min = round(track_length / 100 / 60, digits = 1)) %>%
+  mutate(year = substr(date, 1, 4)) %>%
+  mutate(month = substr(date, 6, 7)) %>%
+  group_by(album) %>%
+  select(album, rank, artist, year, month, track_min, length) %>%
+  mutate(mean_rank = round(mean(rank, na.rm = T), 0),
+  track_length = round(mean(track_min, na.rm = T), 1),
+  num_tracks = round(mean(length, na.rm = T), 0), num_wks = n())
+
+
+function(input, output, session) {
+  
+  # Filter the movies, returning a data frame
+  one <- reactive({
+    # Due to dplyr issue #318, we need temp variables for input values
+    time <- input$one_min_length
+    track <- input$one_track_num
+    minyear <- input$one_year[1]
+    maxyear <- input$one_year[2]
+    minduration <- input$one_time_on[1]
+    maxduration <- input$one_time_on[2]
+    
+    # Apply filters
+    m <- one_data %>%
+      filter(
+        track_length >= time,
+        num_tracks >= track,
+        year >= minyear,
+        year <= maxyear,
+        num_wks >= minduration,
+        num_wks <= maxduration
+      )
+    
+    m <- as.data.frame(m)
+    
+    # Add column which says whether the movie won any Oscars
+    # Be a little careful in case we have a zero-row data frame
+    m$above_10 <- character(nrow(m))
+    m$above_10[m$mean_rank <= 10] <- "Yes"
+    m$above_10[m$mean_rank > 10] <- "No"
+    m
+  })
+  
+  # Function for generating tooltip text
+  movie_tooltip <- function(x) {
+    if (is.null(x)) return(NULL)
+    if (is.null(x$ID)) return(NULL)
+    
+    # Pick out the movie with this ID
+    one_data <- isolate(one_data())
+    one_data_temp <- one_data[one_data$ID == x$ID, ]
+    
+    paste0("<b>", one_data_temp$album, "</b><br>",
+           one_data_temp$artist,"</b>")
+  }
+  
+  # A reactive expression with the ggvis plot
+  vis <- reactive({
+    # Lables for axes
+    xvar_name <- names(axis_vars)[axis_vars == input$xvar]
+    yvar_name <- names(axis_vars)[axis_vars == input$yvar]
+    
+    # Normally we could do something like props(x = ~BoxOffice, y = ~Reviews),
+    # but since the inputs are strings, we need to do a little more work.
+    xvar <- prop("x", as.symbol(input$xvar))
+    yvar <- prop("y", as.symbol(input$yvar))
+    
+    one_data %>%
+      ggvis(x = xvar, y = yvar) %>%
+      layer_points(size := 50, size.hover := 200,
+                   fillOpacity := 0.2, fillOpacity.hover := 0.5,
+                   stroke = ~above_10, key := ~ID) %>%
+      add_tooltip(movie_tooltip, "hover") %>%
+      add_axis("x", title = xvar_name) %>%
+      add_axis("y", title = yvar_name) %>%
+      add_legend("stroke", title = "Average Rank was Above 10", values = c("Yes", "No")) %>%
+      scale_nominal("stroke", domain = c("Yes", "No"),
+                    range = c("sienna2", "saddlebrown")) %>%
+      set_options(width = 500, height = 500)
+  })
+  
+  vis %>% bind_shiny("plot1")
+  
+  output$n_movies <- renderText({ nrow(one_data()) })
+}
+
--- Final Project/Music_Through_Time/question_one.R
+++ Final Project/Music_Through_Time/question_one.R
@@ -1,104 +1,34 @@
-# Data Question One
-library(dplyr)
-library(shiny)
-library(RColorBrewer)
+library(ggvis)
 
-one_data <- read.csv("../../data/billboardalbum200.csv", stringsAsFactors = F)
-colors <- brewer.pal(3, "Accent")
-colors <- list("Seafoam" = colors[1], "Lilac" = colors[2], "Peach" = colors[3])
+axis_vars <- list("Year" = "year", "Rank" = "rank", "Album Length (minutes)" = "track_min")
 
-#create new column with track length in minutes called length_min
-one_data <- one_data %>%
-  mutate(length_min = track_length / 100 / 60)
 
-# This page will explore: How has the length of albums (in minutes)
-# in the Top 200 Per Week changed over time for specific rankings? 
-# Do some number of tracks have a higher liklihood of staying in the
-# billboard 200 for longer? How long OR how many songs are likely for
-# a given year?
-
-# The first graph will: look at all instances of a user chosen rank
-# (for example: 5th place) and using a smoothed line graph show the trend of
-# albumn length (in seconds) over time.
-# The x variable will be time (years)
-# The y variable will be the length_min
-
-one_smooth_sidebar <- sidebarPanel(
-  # Widget grabbing the rank of interest
-  rank_looking_at <- textInput(inputId = "rank_looking_at",
-                          label = "What rank would you like to see? (1-200)"),
-  
-  # Filtering data frame to look at rank of interest 
-  # (i.e. the purpose of the graph), and gather the data into years
-  one_data_rank <- one_data %>%
-    filter(rank == as.integer(rank_looking_at)) %>%
-    mutate(year = substr(date, 1, 4)) %>%
-    group_by(year) %>%
-    summarize(mean_length = mean(length_min, na.rm = TRUE)),
-  
-  # Decide on a color for the graph line
-  selectInput(
-    "one_smooth_color",
-    "Color of line",
-    choices = colors,
-    selected = colors[[2]]
+fluidPage(
+  titlePanel("Do Track Number or Length Matter?"),
+  fluidRow(
+    column(3,
+           wellPanel(
+             h4("Filter Fields"),
+             sliderInput("one_min_length", "Minimum length of album (in minutes)",
+                         3, 800, 100, step = 5),
+             sliderInput("one_track_num", "Minimum number of tracks on album",
+                         0, 700, 18, step = 1),
+             sliderInput("one_year", "Year on Billboard Top 200", 1963, 2019, value = c(1963, 2019),
+                         sep = ""),
+             sliderInput("one_time_on", "Number of weeks on Billboard Top 200",
+                         0, 52, c(0, 52), step = 1)
+           ),
+           wellPanel(
+             selectInput("xvar", "X-axis variable", axis_vars, selected = "year"),
+             selectInput("yvar", "Y-axis variable", axis_vars, selected = "track_min"),
+             tags$small(paste0(
+               "Note: Some help found through movie explorer",
+               "shiny from R studio help graph by Winston Chang"
+             ))
+           )
+    ),
+    column(9,
+           ggvisOutput("plot1")
+    )
   )
 )
-
-one_smooth_main <- mainPanel(plotOutput("one_smooth"))
-
-
-# The second graph will: Look at the number of tracks within the top
-# user selected number of chosen ranked album each week and which number
-# is most popular for each rank
-one_count_sidebar <- sidebarPanel(
-  
-  # Select range of ranks
-  rank_selected <- sliderInput(
-    "one_count_slider",
-    "Up to what rank?",
-    min = 1, max = 200, value = 10
-  ),
-  
-  # Get dataframe of interest
-  one_data_rank <- one_data %>%
-    filter(rank <= rank_selected) %>%
-    select(rank, length),
-  
-  # Select Color of dots
-  selectInput(
-    "one_count_color",
-    "Color of dots",
-    choices = colors,
-    selected = colors[[3]]
-  )
-)
-
-one_count_main <- mainPanel(plotOutput("one_count"))
-
-# The last graph will: bubble plot where year = categorical variable and
-# continuous is either num of tracks OR length in mins
-
-one_scatter_sidebar <- sidebarPanel(
-  radioButtons(
-    "one_scatter_y_var",
-    "What Measurement?",
-    choices = list("Number of tracks" = "length",
-                   "Length of album (minutes)" = "length_min"),
-    selected = "length"
-  ),
-  
-  one_rank <- sliderInput(
-    "one_scatter_rank",
-    "Which Rank?",
-    min = 1, max = 200, value = 1
-  ),
-  
-  one_data_rank <- one_data %>%
-    filter(rank == one_rank)
-)
-  
-
-
-
-
