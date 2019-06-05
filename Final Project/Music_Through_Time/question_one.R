eddc52639a59c9d07b3e96129e56ca1545eac8ff
Sophie W <sfw6@uw.edu>
2019-06-04 23:59:21 -0700

    labels?

--- Final Project/Music_Through_Time/question_one.R
+++ Final Project/Music_Through_Time/question_one.R
@@ -45,7 +45,7 @@ display <-  selectInput( "y_var_1", "By length in minutes or tracks",
 
 # Create tabPanel for the page
 response_one <- tabPanel(
-  "Page 1",
+  "Data By Length",
   titlePanel("How Length (Number and Time) Relate to Popularity"),
   sidebarLayout(
     sidebarPanel(
@@ -60,38 +60,3 @@ response_one <- tabPanel(
     )
   )
 )
-
-
-
-
-# 
-# navbarPage(
-#   title = ("Do Track Number or Length Matter?"),
-#   sidebarPanel(
-#     column(3,
-#            wellPanel(
-#              h4("Filter Fields"),
-#              sliderInput("one_min_length", "Minimum length of album (in minutes)",
-#                          3, 800, 100, step = 5),
-#              sliderInput("one_track_num", "Minimum number of tracks on album",
-#                          0, 700, 18, step = 1),
-#              sliderInput("one_year", "Year on Billboard Top 200", 1963, 2019, value = c(1963, 2019),
-#                          sep = ""),
-#              sliderInput("one_time_on", "Number of weeks on Billboard Top 200",
-#                          0, 52, c(0, 52), step = 1)
-#            ),
-#            wellPanel(
-#              selectInput("xvar", "X-axis variable", axis_vars, selected = "year"),
-#              selectInput("yvar", "Y-axis variable", axis_vars, selected = "track_min"),
-#              tags$small(paste0(
-#                "Note: Some help found through movie explorer",
-#                "shiny from R studio help graph by Winston Chang"
-#              ))
-#            )
-#     ),
-#     column(9,
-#            ggvisOutput("plot1")
-#     )
-#   )
-# )
-
